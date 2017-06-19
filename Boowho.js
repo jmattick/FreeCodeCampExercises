cd0ac7e310cab6b3fc40de8e1c7d685ed12f6b91
Jessica M <jmatty16@users.noreply.github.com>
2017-06-19 14:43:44 -0500

    Create Boowho.js

---
+++ Boowho.js
@@ -0,0 +1,10 @@
+//Check if a value is classified as a boolean primitive. Return true or false.
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
