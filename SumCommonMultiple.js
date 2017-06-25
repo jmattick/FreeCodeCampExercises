dbf63abab9388df1f8a33bca5897a17b5a6dcd64
Jessica M <jmatty16@users.noreply.github.com>
2017-06-25 12:42:49 -0500

    Create SumCommonMultiple.js

---
+++ SumCommonMultiple.js
@@ -0,0 +1,23 @@
+//Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+function smallestCommons(arr) {
+    var range = [];
+    for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+    range.push(i);
+    }
+
+   
+    var lcm = range[0];
+    for (i = 1; i < range.length; i++) {
+    var GCD = gcd(lcm, range[i]);
+    lcm = (lcm * range[i]) / GCD;
+    }
+    return lcm;
+
+    function gcd(x, y) {    // Implements the Euclidean Algorithm
+    if (y === 0)
+        return x;
+    else
+        return gcd(y, x%y);
+    }
+}
